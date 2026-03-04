89f8fe0308910c7c78c00561dd78e5e8c52b39f9
Alkasa830 <nezinau3324@gmail.com>
2026-03-04 15:04:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
 
+// Render apgavystė, kad veiktų nemokamai
+const app = express();
+app.get('/', (req, res) => res.send('Botas gyvas!'));
+app.listen(process.env.PORT || 3000);
+
+// Boto smegenys
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once('ready', () => {
+    console.log('ShadowWolf išlindo iš miško!');
+});
+
+client.login(process.env.BOT_TOKEN);
